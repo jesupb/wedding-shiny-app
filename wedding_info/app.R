7d790f4060de5eb935d3908ca9bdb7a49feedee8
jesupb <139jjpb@gmail.com>
2021-03-30 18:38:08 -0600

    update sitting distro

--- wedding_info/app.R
+++ wedding_info/app.R
@@ -44,15 +44,6 @@ server <- function(input, output) {
     
     filedata <- read.csv(url("https://raw.githubusercontent.com/jesupb/wedding-shiny-app/main/wedding_info/sitting_distro.csv"))
     
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-    
     output$mytable = DT::renderDataTable({
         filedata
     })
